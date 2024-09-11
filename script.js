605505794b12b4f1e24b07680b08a1cbabc2b4a1
Unuafe Ajiri <aj4short@rocketmail.com>
2024-09-11 04:39:45 +0100

    create a random color function and the divs within the container

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 let pageTitle = document.createElement('h1');
-pageTitle.textContent = 'Etch As Sketch';
+pageTitle.textContent = 'Etch A Sketch';
 pageTitle.style.textAlign = 'center';
 
 let pageBody = document.querySelector('body');
 pageBody.appendChild(pageTitle)
 
-container.style.width = '500px';
-container.style.height = '500px';
+let container = document.querySelector('#container')
+let width = '500px'
+let height = '500px'
+container.style.width = `${width}`;
+container.style.height = `${height}`;
 container.style.margin = 'auto';
-container.style.border = '1px solid black';
-
 pageBody.insertBefore(pageTitle, container);
+container.style.display = 'flex';
+container.style.flexWrap = 'wrap'
+let num = 19
 
-
-
-div.classList.add('box');
-div.style.width = '20px'
-div.style.height = '20px'
-div.style.backgroundColor = 'green'
-
-for(let i = 0; i < 16; i++){
-    let div = document.createElement('div');
-    container.appendChild(div);
+function randomColor(){
+    let color = ['orange', 'green', 'pink', 'purple', 'green', 'blue', 'black', 'gray', 'yellow', 'skyblue'];
+    let randomValue = Math.floor(Math.random() * color.length)
+    return color[randomValue]
 }
 
+for(let i = 0; i < num * num; i++){
+    let div = document.createElement('div');
+    div.style.height = `calc(${height} / ${num})`
+    div.style.width = `calc(${width} / ${num})`
+    div.style.boxShadow = '0px 0px 0px 1px black inset';
+    container.appendChild(div);
+   
+}
